b073c8e9999a2c32655b4c64681892cf57e6c224
Cyril Cressent <cyril@cressent.org>
2017-03-28 16:46:00 -0700

    Factor out storage/retrieval into Redis

--- src/Main.hs
+++ src/Main.hs
@@ -8,8 +8,12 @@ import Data.Text.Encoding (encodeUtf8, decodeUtf8)
 import qualified Database.Redis as R
 import Web.Scotty
 
+import qualified Data.ByteString.Char8 as BS
 import qualified Data.Text.Lazy as TL
 import qualified System.Random as SR
+
+type Key = TL.Text
+type URL = TL.Text
 
 alphanum :: String
 alphanum = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
@@ -23,22 +27,41 @@ shortener :: R.Connection -> ScottyM ()
 shortener conn = do
     get "/" $ do
         url    <- param "url"
-        key    <- fmap TL.pack $ liftAndCatchIO $ random 16 alphanum
-        result <- liftAndCatchIO $ R.runRedis conn $ R.set (encodeUtf8 (TL.toStrict key)) url
+        result <- liftAndCatchIO $ store conn url
 
         case result of
-          Left reply   -> text $ TL.pack $ show reply
-          Right R.Ok   -> text $ "Your shortened URL: http://localhost:3000/" <> key
-          Right status -> text $ "Unexpected error: " <> TL.pack (show status)
+          Left reply -> text $ TL.pack $ show reply
+          Right key  -> html $ "Your shortened URL: <a href=\"" <> shortURL <> "\">" <> shortURL <> "</a>"
+            where shortURL = keyToShortURL key
 
     get "/:key" $ do
         key    <- param "key"
-        result <- liftAndCatchIO $ R.runRedis conn $ R.get key
+        result <- liftAndCatchIO $ retrieve conn key
 
         case result of
           Left reply       -> text $ TL.pack $ show reply
-          Right (Just url) -> text $ TL.fromStrict $ decodeUtf8 url
-          Right Nothing    -> text $ "key " <> TL.fromStrict (decodeUtf8 key) <> " not found"
+          Right (Just url) -> redirect url
+          Right Nothing    -> text $ "key " <> key <> " not found"
+
+store :: R.Connection -> URL -> IO (Either R.Reply Key)
+store conn url = do
+  key    <- random 16 alphanum
+  result <- R.runRedis conn $ R.set (BS.pack key) url'
+  case result of
+    Left reply -> return $ Left reply
+    Right _    -> return $ Right (TL.pack key)
+  where url' = (encodeUtf8 . TL.toStrict) url
+
+retrieve :: R.Connection -> Key -> IO (Either R.Reply (Maybe URL))
+retrieve conn key = do
+  result <- R.runRedis conn $ R.get key'
+  case result of
+    Left reply -> return $ Left reply
+    Right url  -> return $ Right $ fmap (TL.fromStrict . decodeUtf8) url
+  where key' = encodeUtf8 $ TL.toStrict key
+
+keyToShortURL :: Key -> URL
+keyToShortURL key = "http://localhost:3000/" <> key
 
 random :: Int -> [a] -> IO [a]
 random n xs = replicateM n (randomElement xs)
